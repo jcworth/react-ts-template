b4156963334fa6445e21a74ebd145cea3a2fc41e
Jack Holdsworth <jackch90@gmail.com>
2021-01-24 12:19:01 +0000

    initial commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './app.css';
+
+const App = () => (
+    <div>
+        <p>Hello World</p>
+    </div>
+);
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        main: path.resolve(__dirname, './src/index.js')
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/index.html')
+        }),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    devServer: {
+        contentBase: path.resolve(__dirname, './dist'),
+        publicPath: '/',
+        open: true,
+        compress: true,
+        hot: true,
+        port: 8080
+    }
+};
